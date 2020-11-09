519a6121f52bbd4d784a5a501a19e07b431d6b4e
Quin Thompson <cleveland.thompson@mirpartners.com>
2020-11-08 22:12:53 -0700

    wip mod2 assignment2

--- Assignments/Module02/SupportCode/Framework-2-DecisionTree.py
+++ Assignments/Module02/SupportCode/Framework-2-DecisionTree.py
@@ -1,46 +1,79 @@
 import MachineLearningCourse.MLUtilities.Learners.DecisionTree as DecisionTree
+import MachineLearningCourse.MLSolution.ParameterSweep as ParameterSweep
+import MachineLearningCourse.MLUtilities.Data.Sample as Sample
+import MachineLearningCourse.MLProjectSupport.Adult.AdultDataset as AdultDataset
+import MachineLearningCourse.Assignments.Module02.SupportCode.AdultFeaturize as AdultFeaturize
+
+
+(xRaw, yRaw) = AdultDataset.LoadRawData()
+
+
+(xTrainRaw, yTrain, xValidateRaw, yValidate, xTestRaw,
+ yTest) = Sample.TrainValidateTestSplit(xRaw, yRaw)
+
+print("Train is %d samples, %.4f percent >50K." %
+      (len(yTrain), 100.0 * sum(yTrain)/len(yTrain)))
+print("Validate is %d samples, %.4f percent >50K." %
+      (len(yValidate), 100.0 * sum(yValidate)/len(yValidate)))
+print("Test is %d samples %.4f percent >50K." %
+      (len(yTest), 100.0 * sum(yTest)/len(yTest)))
+
+modelDefaults = {
+    'maxDepth': 5
+}
+
+featurizerDefaults = {
+    'useCategoricalFeatures': True,
+    'useNumericFeatures': False
+}
+
+ParameterSweep.hyperparameterSweep('maxDepth', xTrainRaw, yTrain, modelType=DecisionTree.DecisionTree,
+                                   featurizerType=AdultFeaturize.AdultFeaturize, featureCreateMethod='CreateFeatureSet', paramValues=[1, 3, 5, 8, 10, 15, 20], modelDefaults=modelDefaults, featurizerDefaults=featurizerDefaults)
+
 
 # Some simple test cases to get you started. You'll have to work out the correct answers yourself.
+simpleTests = False
 
-print("test simple split")
-x = [[1], [1], [1], [1], [1], [0], [0], [0], [0], [0]]
-y = [1, 1, 1, 1, 1, 1, 0, 0, 0, 1]
-model = DecisionTree.DecisionTree()
-model.fit(x, y)
+if simpleTests:
+    print("test simple split")
+    x = [[1], [1], [1], [1], [1], [0], [0], [0], [0], [0]]
+    y = [1, 1, 1, 1, 1, 1, 0, 0, 0, 1]
+    model = DecisionTree.DecisionTree()
+    model.fit(x, y)
 
-model.visualize()
+    model.visualize()
 
-print("test no split")
-x = [[1], [1], [1], [1], [1], [0], [0], [0], [0], [0]]
-y = [1, 1, 0, 1, 1, 1, 1, 1, 0, 1]
-model = DecisionTree.DecisionTree()
-model.fit(x, y)
+    print("test no split")
+    x = [[1], [1], [1], [1], [1], [0], [0], [0], [0], [0]]
+    y = [1, 1, 0, 1, 1, 1, 1, 1, 0, 1]
+    model = DecisionTree.DecisionTree()
+    model.fit(x, y)
 
-model.visualize()
+    model.visualize()
 
-print("test numeric feature sort")
-x = [[1, 3], [2, 2], [19, 7], [4, 1]]
-y = [1, 1, 0, 0]
+    print("test numeric feature sort")
+    x = [[1, 3], [2, 2], [19, 7], [4, 1]]
+    y = [1, 1, 0, 0]
 
-model = DecisionTree.DecisionTree()
-model.fit(x, y)
+    model = DecisionTree.DecisionTree()
+    model.fit(x, y)
 
-model.visualize()
+    model.visualize()
 
-print("Bigger tree")
-x = [[10, 7], [9, 8], [101, 71], [44, 44], [19, 111],
-     [1, 2], [1, 3], [2, 2], [19, 7], [4, 1]]
-y = [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
+    print("Bigger tree")
+    x = [[10, 7], [9, 8], [101, 71], [44, 44], [19, 111],
+         [1, 2], [1, 3], [2, 2], [19, 7], [4, 1]]
+    y = [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
 
-model = DecisionTree.DecisionTree()
-model.fit(x, y)
+    model = DecisionTree.DecisionTree()
+    model.fit(x, y)
 
-model.visualize()
+    model.visualize()
 
 # These might help you debug...
-doVisualize = True
+doVisualize = False
 if doVisualize:
-    kOutputDirectory = "C:\\Users\\ghult\\Desktop\\visualize"
+    kOutputDirectory = "./temp/mod2/assignment2"
 
     import MachineLearningCourse.MLUtilities.Data.Generators.SampleUniform2D as SampleUniform2D
     import MachineLearningCourse.MLUtilities.Data.Generators.ConceptCircle2D as ConceptCircle2D
@@ -57,17 +90,17 @@ if doVisualize:
 
     import MachineLearningCourse.MLUtilities.Visualizations.Visualize2D as Visualize2D
 
-    visualize = Visualize2D.Visualize2D(kOutputDirectory, "Generated Concept")
+    visualize = Visualize2D.Visualize2D(kOutputDirectory, "generated-concept")
 
     visualize.Plot2DDataAndBinaryConcept(x, y, concept)
     visualize.Save()
 
     model = DecisionTree.DecisionTree()
-    model.fit(x, y, maxDepth=2)
+    model.fit(x, y, maxDepth=100)
     model.visualize()
 
     visualize = Visualize2D.Visualize2D(
-        kOutputDirectory, "DecisionTree on Generated Concept")
+        kOutputDirectory, "decision-tree")
 
     visualize.Plot2DDataAndBinaryConcept(x, y, model)
     visualize.Save()
--- MLUtilities/Learners/DecisionTree.py
+++ MLUtilities/Learners/DecisionTree.py
@@ -1,11 +1,63 @@
 import collections
+from collections import Counter
 import math
 import time
+from math import log2
+from tabulate import tabulate
 
 
-def Entropy(y):
-    print("Stub Entropy in ", __file__)
-    return 0.0
+def printFeatureData(x, y, featureIndex):
+    indexesInSortedOrder = sorted(
+        range(len(x)), key=lambda i: x[i][featureIndex])
+
+    sortedX = list(map(lambda i: x[i], indexesInSortedOrder))
+    sortedY = list(map(lambda i: y[i], indexesInSortedOrder))
+
+    table = []
+    for i in range(len(sortedX)):
+        row = []
+        row.extend(sortedX[i])
+        row.append(sortedY[i])
+        table.append(row)
+
+    headers = ["x%s" % (i) for i in range(len(x[0]))]
+    headers.append('y')
+
+    print(tabulate(table, headers, tablefmt="github"))
+
+
+def Entropy(y: []):
+    counter = Counter(y)
+    totalCount = float(len(y))
+    result = 0
+    for (_, count) in counter.items():
+        count = float(count)
+        p = count/totalCount
+        result += p * log2(p)
+
+    return result * -1.0
+
+
+def SplitEntropy(x, y, index):
+    result = 0
+    fHX = x[:index]
+    sHX = x[index:]
+    firstHalf = y[:index]
+    secondHalf = y[index:]
+
+    ys = [firstHalf, secondHalf]
+    for ySection in ys:
+        result += Entropy(ySection) * float(len(ySection))
+    entropy = result/float(len(y))
+    splitX = [fHX, sHX]
+    splitY = [firstHalf, secondHalf]
+    return (entropy, splitX, splitY)
+
+
+def InformationGain(entropyY, x, y, index):
+    (entropy, splitX, splitY) = SplitEntropy(x, y, index)
+    IG = entropyY - entropy
+    return (IG, splitX, splitY)
 
 
 def FindBestSplitOnFeature(x, y, featureIndex):
@@ -15,19 +67,50 @@ def FindBestSplitOnFeature(x, y, featureIndex):
 
     # Do more tests to make sure you haven't hit a terminal case...
 
-    # HINT here is how to get an array that has indexes into the data (x, y) arrays in sorded
-    # order based on the value of the feature at featureIndex
     indexesInSortedOrder = sorted(
         range(len(x)), key=lambda i: x[i][featureIndex])
 
-    # so x[indexesInSortedOrder[0]] will be the training sample with the smalles value of 'featureIndex'
-    # and y[indexesInSortedOrder[0]] will be the associated label
+    sortedX = list(map(lambda i: x[i], indexesInSortedOrder))
+    sortedY = list(map(lambda i: y[i], indexesInSortedOrder))
 
-    print("Stub FindBestSplitOnFeature in ", __file__)
+    totalEntropy = Entropy(y)
 
-    # HINT: might like to return the partitioned data and the
-    #  entropy after partitioning based on the threshold
-    return (bestThreshold, splitData, entropyAfterSplit)
+    bestThreshold = 0
+    splitData = []
+    gain = 0
+    lastValidIndex = None
+
+    for i in range(len(x) - 1):
+        (currY, nextY) = (sortedY[i], sortedY[i+1])
+        (currX, nextX) = [sortedX[i][featureIndex], sortedX[i+1][featureIndex]]
+
+        if (currX != nextX):
+            # we could split here if we wanted to
+            lastValidIndex = i
+        if (currX != nextX and currY == nextY):
+            # this is not optimal. We can continue searching (though we could calculate)
+            continue
+
+        if lastValidIndex == None:
+            # is this bad?
+            continue
+
+        (IG, splitX, splitY) = InformationGain(
+            totalEntropy, sortedX, sortedY, lastValidIndex+1)
+        if (IG > gain):
+            bestThreshold = (float(currX + nextX))/2.0
+            splitData = [splitX, splitY]
+            gain = IG
+
+    if lastValidIndex != None:
+        (IG, splitX, splitY) = InformationGain(
+            totalEntropy, sortedX, sortedY, lastValidIndex+1)
+        if (IG > gain):
+            bestThreshold = (float(currX + nextX))/2.0
+            splitData = [splitX, splitY]
+            gain = IG
+
+    return (bestThreshold, splitData, gain)
 
 
 class TreeNode(object):
@@ -52,15 +135,62 @@ class TreeNode(object):
 
     def growTree(self, maxDepth):
         if self.depth == maxDepth:
-            return
+            # max recursion depth
+            return self
 
-        print("Stub growTree in ", __file__)
+        if len(self.labelDistribution.items()) <= 1:
+            # leaf. TODO: maybe explicitly set, probably just leave as is
+            return self
+
+        (maxFeature, bestThreshold, splitData, IG) = self.bestSplitAttribute()
+        if IG == 0:
+            # terminate
+            return self
+
+        self.splitIndex = maxFeature
+        self.threshold = bestThreshold
+
+        leftNode = TreeNode(self.depth+1)
+        rightNode = TreeNode(self.depth+1)
+
+        [splitX, splitY] = splitData
+
+        leftNode.addData(splitX[0], splitY[0])
+        rightNode.addData(splitX[1], splitY[1])
+        leftNode.growTree(maxDepth)
+        rightNode.growTree(maxDepth)
+        self.children = [leftNode, rightNode]
+
+        return self
+
+    def bestSplitAttribute(self):
+        informationGains = {}
+        results = {}
+
+        for i in range(len(self.x[0])):
+            (bestThreshold, splitData, IG) = FindBestSplitOnFeature(
+                self.x, self.y, i)
+            informationGains[i] = IG
+            results[i] = (bestThreshold, splitData, IG)
+
+        all0 = all(value == 0 for value in informationGains.values())
+        if all0:
+            # Not 100p on this one
+            return (None, None, None, 0)
+
+        maxFeature = max(informationGains, key=informationGains.get)
+        (bestThreshold, splitData, IG) = results[maxFeature]
+        return (maxFeature, bestThreshold, splitData, IG)
 
     def predictProbability(self, x):
         # Remember to find the correct leaf then use an m-estimate to smooth the probability:
         #  (#_with_label_1 + 1) / (#_at_leaf + 2)
-
-        print("Stub predictProbability in ", __file__)
+        if (self.isLeaf()):
+            return (float(self.labelDistribution[1] + 1))/float(len(self.y) + 2)
+        if x[self.splitIndex] < self.threshold:
+            return self.children[0].predictProbability(x)
+        else:
+            return self.children[1].predictProbability(x)
 
     def visualize(self, depth=1):
         # Here is a helper function to visualize the tree (if you choose to use the framework class)
@@ -71,7 +201,7 @@ class TreeNode(object):
             print("Split on: %d" % (self.splitIndex))
 
             # less than
-            for i in range(depth):
+            for _ in range(depth):
                 print(' ', end='', flush=True)
             print("< %f -- " % self.threshold, end='', flush=True)
             self.children[0].visualize(depth+1)
